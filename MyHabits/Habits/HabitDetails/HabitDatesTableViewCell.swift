e0c90124171214d0682729976781e3a344e7c0ad
AlenAvako <alen.madwatch@gmail.com>
2021-11-10 20:14:56 +0300

    Проверка

--- MyHabits/Habits/HabitDetails/HabitDatesTableViewCell.swift
+++ MyHabits/Habits/HabitDetails/HabitDatesTableViewCell.swift
@@ -8,29 +8,5 @@
 import UIKit
 
 class HabitDatesTableViewCell: UITableViewCell {
-    
-    static let id = "HabitDatesTableViewCell"
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.selectionStyle = .none
-        
-        contentView.addSubviews(trackDates)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    lazy var trackDates: UILabel = {
-        let label = UILabel()
-        label.toAutoLayout()
-        label.font = UIFont.systemFont(ofSize: 17)
-        label.textColor = .black
-        return label
-    }()
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-    }
 }
